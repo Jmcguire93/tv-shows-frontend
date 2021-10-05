e2190cbad0402903cfaa9c2502062373325dcef3
James <james.ryan.mcguire@gmail.com>
2021-10-05 15:01:55 -0400

    Removes more details link

--- src/views/ShowsIndex.vue
+++ src/views/ShowsIndex.vue
@@ -16,7 +16,6 @@
       <p>Favorite: {{ show.favorite }}</p>
       <!-- <p>Image: {{ show.image }}</p> -->
       <p>User: {{ show.user.name }}</p>
-      <router-link v-bind:to="`/shows/${show.id}`">More details</router-link>
     </div>
   </div>
 </template>
